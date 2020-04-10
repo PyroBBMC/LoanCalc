69b840d5708f2b6573259e493da18ab06353d067
Toriano Triche <torianotriche@gmail.com>
2020-04-10 14:53:57 -0400

    more changes

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function calculateResults() {
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
 
-    // Show resultss
+    // Show resultsss
     document.getElementById("results").style.display = "block";
 
     // Hide loader
